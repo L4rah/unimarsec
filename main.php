79a5e12a578ed83fb2f31124ae48dec71a1fa9f9
Larah <lmallavazi@gmail.com>
2023-06-08 15:59:39 -0300

    novo logo

---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <img src="img.png" alt="logo da home host" style="float: right;width: 200px;">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unimar Security</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <div class="page">
+        <form method="POST" class="formLogin">
+            <input type="submit" value="LOGIN ESTUDANTE" class="btn" />
+            <input type="submit" value="LOGIN COLABORADOR" class="btn" />
+            <input type="submit" value="REGISTRAR-SE" class="btn" />
+        </form>
+    </div>
+</body>
+
+</html>
